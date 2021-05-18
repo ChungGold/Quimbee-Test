bcf159d38e841e3b35ed0dbcee4f0fdcd1101936
chunggold <chungau@csu.fullerton.edu>
2021-05-18 13:09:12 -0700

    added props, padding, margins, sizing, and displays

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import Card from './Card';
+import Reviewers from './Reviewers';
 
-// const createCard = (Projects) => {
-//     return (
-//         <Card 
-//         image = {Projects.image}
-//         alt = {Projects.alt}
-//         title = {Projects.title}
-//         desc = {Projects.desc}
-//         github = {Projects.github}
-//         link = {Projects.link}
-//     />
-//     )
-// }
+const createCard = (Reviewers) => {
+    return (
+        <Card 
+        image = {Reviewers.image}
+        alt = {Reviewers.alt}
+        name = {Reviewers.name}
+        verified = {Reviewers.verified}
+        summary = {Reviewers.summary}
+        description = {Reviewers.description}
+        rating = {Reviewers.rating}
+    />
+    )
+}
 
 const App = props => {
     return (<div id = 'app'>
-        <Card />
+        {Reviewers.map(createCard)}
     </div>);
 }
 
---
+++ src/Reviewers.jsx
@@ -0,0 +1,15 @@
+import chung from './assets/chung.png';
+
+const Reviewers = [
+    {
+        image: chung,
+        alt: "Chung Au",
+        name: "Chung Au",
+        verified: "Verified purchaser",
+        summary: "Quimbee is the best thing ever. And this goes to two lines.",
+        description: "I love Quim bee. It saved me in the 1L and it's saving me in 2L. I love it all and it's money well spent.",
+        rating: "4 out of 5",
+    }
+]
+
+export default Reviewers;
