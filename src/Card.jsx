75f7553ba799d2e5166333e9a39dd86c4907d334
chunggold <chungau@csu.fullerton.edu>
2021-05-18 13:41:08 -0700

    finalized styling

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,18 +1,23 @@
+import {HiOutlineShieldCheck, HiStar, HiOutlineStar} from 'react-icons/hi';
+
 const Card = props => {
     return (<div className = 'card'>
         <figure className = 'reviewer'>
-            <img src = {props.image} alt = {props.alt} className = 'profile'></img>
+            <img src = {props.image} alt = {props.alt} className = 'picture'></img>
             <div className = 'reviewer-info'>
                 <figcaption className = 'name'>{props.name}</figcaption>
-                <figcaption className = 'verified'>{props.verified}</figcaption>
+                <figcaption className = 'verified'><HiOutlineShieldCheck />{props.verified}</figcaption>
             </div>
         </figure>
         <figcaption className = 'card-info'>
             <p className = 'summary'>{props.summary}</p>
             <p className = 'description'>{props.description}</p>
-            <p>{props.rating}</p>
-            <p>for<span>Quimbee Bar Review</span></p>
-            <p>Reviwed 3 days ago</p>
+            <p>
+                <span className = 'stars'><HiStar /><HiStar /><HiStar /><HiStar /><HiOutlineStar /></span>
+                {props.rating}
+            </p>
+            <p className = 'for'>for <span className = 'quimbee'>Quimbee Bar Review</span></p>
+            <p className = 'days'>Reviewed 3 days ago</p>
         </figcaption>
     </div>);
 }
--- src/Reviewers.jsx
+++ src/Reviewers.jsx
@@ -5,10 +5,10 @@ const Reviewers = [
         image: chung,
         alt: "Chung Au",
         name: "Chung Au",
-        verified: "Verified purchaser",
+        verified: " Verified purchaser",
         summary: "Quimbee is the best thing ever. And this goes to two lines.",
-        description: "I love Quim bee. It saved me in the 1L and it's saving me in 2L. I love it all and it's money well spent.",
-        rating: "4 out of 5",
+        description: "I love Quimbee. It saved me in the 1L and it's saving me in 2L. I love it all and it's money well spent.",
+        rating: " 4 out of 5",
     }
 ]
 
