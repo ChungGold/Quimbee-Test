b252c38d4d8ecdb776b9883f034114ef3d8599b2
chunggold <chungau@csu.fullerton.edu>
2021-05-26 23:46:31 -0700

    added in another card and fixed scrollbar

--- src/components/Reviewers.jsx
+++ src/components/Reviewers.jsx
@@ -1,40 +1,52 @@
 import chung from '../assets/chung.png';
-import matthew from '../assets/matthew.PNG';
+import ryan from '../assets/ryan.jpg';
 import rogan from '../assets/rogan.png';
+import bua from '../assets/bua.jpg';
 
 const Reviewers = [
-    {
-        image: matthew,
-        alt: "Matthew Folkner",
-        name: "Matthew Folkner",
-        verified: true,
-        summary: "Quimbee is the best thing ever. And this goes to two lines.",
-        description: "I love Quimbee. It saved me in the 1L and it's saving me in 2L. I love it all and it's money well spent.",
-        rating: 4,
-        daysAgo: 3,
-        business: "Quimbee Bar Review"
-    },
     {
         image: chung,
         alt: "Chung Au",
         name: "Chung Au",
-        verified: false,
+        verified: true,
         summary: "I am excited to interview as a frontend developer!",
         description: "I took a few extra steps in this technical interview. I hope it impresses!",
         rating: 5,
         daysAgo: 2,
-        business: "Quimbee Bar Review"
+        business: "Technical Assignment"
+    },
+    {
+        image: ryan,
+        alt: "Ryan Hall",
+        name: "Ryan Hall",
+        verified: true,
+        summary: "I can endorse Chung.",
+        description: "I don't really know the guy, but anybody that watches my interviews is solid in my book.",
+        rating: 4,
+        daysAgo: 3,
+        business: "Fifty/50 BJJ"
     },
     {
         image: rogan,
         alt: "Joe Rogan",
         name: "Joe Rogan",
-        verified: true,
-        summary: "I would recommend both Quimbee and Chung Au",
+        verified: false,
+        summary: "I would recommend Chung Au",
         description: "Chung Au is a capable frontend developer with a sense of fun. I have never worked with him, but I just know it.",
         rating: 3,
         daysAgo: 6,
         business: "Joe Rogan Experience"
+    },
+    {
+        image: bua,
+        alt: "Buakaw Banchamek",
+        name: "Buakaw Banchamek",
+        verified: false,
+        summary: "I don't understand",
+        description: "I'm not sure why I was chosen as a review-card. I'm a monster in Muay Thai though.",
+        rating: 2,
+        daysAgo: 6,
+        business: "Banchamek Gym"
     }
 ]
 
